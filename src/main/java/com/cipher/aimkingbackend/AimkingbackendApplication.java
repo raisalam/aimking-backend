3347dbd3598571e6e229280b0d0ba14f79a4b9b9
Rais Alam <rais@cloudeagle.ai>
2024-11-03 10:35:22 +0530

    first commit

---
+++ src/main/java/com/cipher/aimkingbackend/AimkingbackendApplication.java
@@ -0,0 +1,13 @@
+package com.cipher.aimkingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AimkingbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AimkingbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cipher/aimkingbackend/AimkingbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cipher.aimkingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AimkingbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
